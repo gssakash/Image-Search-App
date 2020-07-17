5830a9498c4d17a83e94ab1260f2534737d35264
Akash <gsaisriram.akash@gmail.com>
2020-07-17 16:56:22 +0530

    finished

--- src/components/ImageList/ImageList.js
+++ src/components/ImageList/ImageList.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 import "./ImageList.css"
 
 const ImageList = ({ images }) => {
-  console.log(images)
+  //console.log(images)
   return (
     <div className="container">
       <div className="row">
